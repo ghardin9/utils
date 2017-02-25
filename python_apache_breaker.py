dce8656b79eee73665b648897d1bad547bd9579c
root <root@localhost.localdomain>
2017-02-25 01:53:19 -0500

    wrapped initial script in function

--- python_apache_breaker.py
+++ python_apache_breaker.py
@@ -1,13 +1,18 @@
 import requests
 
-address = 'http://54.202.82.13/'
-paramcount = 8000
-data = {'name':'a'*paramcount}
-r = requests.get(address, params=data)
-while r.status_code != 414:
-    if paramcount % 1000 == 0:
-        print paramcount
-    paramcount += 1
+def test(address):
+    paramcount = 8000
     data = {'name':'a'*paramcount}
     r = requests.get(address, params=data)
-print paramcount + 19
+    while r.status_code != 414:
+        if paramcount % 1000 == 0:
+            print paramcount
+        paramcount += 1
+        data = {'name':'a'*paramcount}
+        r = requests.get(address, params=data)
+    return paramcount + 19
+
+
+if __name__ == '__main__':
+    address = 'http://54.202.82.13/'
+    print test(address)
